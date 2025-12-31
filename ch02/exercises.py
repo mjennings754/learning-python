a0646205b7b0402944be334095a5c2b45b6bacb3
mjennings754 <mitchellj@outlook.jp>
2025-12-31 19:33:29 +1100

    add exercises from ch02

---
+++ ch02/exercises.py
@@ -0,0 +1,12 @@
+# 2.1 Assign the integer value 99 to the variable prince, and print it.
+prince = 99
+print(prince)
+
+# 2.2 What type is the value 5?
+print(type(5)) #int
+
+# 2.3 What type is the value 2.0?
+print(type(2.0)) #float
+
+# 2.4 What type is the expression 5 + 2.0?
+print(type(5 + 2.0)) #float
